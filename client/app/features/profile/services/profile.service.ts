a88abcc5ee043be36339a3de3c7afa399de8b4d6
Lucio Aimar <lucioaimar12@gmail.com>
2023-03-06 10:46:08 -0300

    Added docstring for services

--- client/app/features/profile/services/profile.service.ts
+++ client/app/features/profile/services/profile.service.ts
@@ -1,5 +1,9 @@
 import { environment } from './../../../../environments/environment';
-import { Payment, Purchase, Shipment } from './../interfaces/purchase.interface';
+import {
+  Payment,
+  Purchase,
+  Shipment,
+} from './../interfaces/purchase.interface';
 import { UserRestriction, Level } from './../interfaces/user.interface';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
@@ -13,16 +17,32 @@ import { PaginatedResponse } from 'client/app/shared/interfaces/pagination.inter
 export class ProfileService {
   http = inject(HttpClient);
 
+  /**
+   * Returns the user
+   * @returns User */
   getUser(): Observable<User> {
     return this.http.get<User>(`${environment.backendUrl}/getUser`);
   }
 
+  /**
+   * Returns all the user restrictions for the user with a given ID.
+   *
+   * @param id The ID of the user whose restrictions will be returned.
+   * @returns An observable of the user restrictions.
+   */
   getUserRestrictions(id: number): Observable<UserRestriction[]> {
     return this.http.get<UserRestriction[]>(
       `${environment.backendUrl}/getUserRestrictions/${id}`
     );
   }
 
+  /**
+   * getUserPurchases - Get all purchases made by a user
+   * @param id - The user's id
+   * @param page - The page number
+   * @param limit - The number of items to return
+   * @returns A paginated list of purchases
+   */
   getUserPurchases(
     id: number,
     page: number,
@@ -37,14 +57,28 @@ export class ProfileService {
     );
   }
 
+  /**
+   *getLevel - Gets the MercadoPuntos level based on a level id.
+   *@param id - The level id
+   */
   getLevel(id: string): Observable<Level> {
     return this.http.get<Level>(`${environment.backendUrl}/getLevel/${id}`);
   }
 
+  /**
+   *getLevel - Gets the shipment object with it's status.
+   *@param id - The shipment id
+   */
   getShipment(id: number): Observable<Shipment> {
-    return this.http.get<Shipment>(`${environment.backendUrl}/getShipment/${id}`);
+    return this.http.get<Shipment>(
+      `${environment.backendUrl}/getShipment/${id}`
+    );
   }
 
+  /**
+   *getLevel - Gets the payment object with it's status.
+   *@param id - The transaction id
+   */
   getPayment(id: number): Observable<Payment> {
     return this.http.get<Payment>(`${environment.backendUrl}/getPayment/${id}`);
   }
--- client/app/global.service.ts
+++ client/app/global.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class GlobalService {
+
+  //This will be useful to use as a store for the errors captured on the interceptor
   error$: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(
     null
   );
